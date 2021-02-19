c860e56a9fb47a3594db7ecef68ffb260006ae26
cyrushx <cyrushx@gmail.com>
2021-02-18 23:57:24 -0500

    plot policies

--- scripts/run_policy.py
+++ scripts/run_policy.py
@@ -1,32 +1,41 @@
-from rlkit.samplers.rollout_functions import rollout
-from rlkit.torch.pytorch_util import set_gpu_mode
 import argparse
+import os
 import torch
 import uuid
+
+from rlkit.samplers.rollout_functions import rollout
+from rlkit.torch.pytorch_util import set_gpu_mode
 from rlkit.core import logger
+from data_generator.maze.utils import plot_problem_path, plot_problem_paths
 
 filename = str(uuid.uuid4())
 
 
 def simulate_policy(args):
     data = torch.load(args.file)
+    fig_dir = os.path.dirname(args.file)
+    print(fig_dir)
     policy = data['evaluation/policy']
     env = data['evaluation/env']
     print("Policy loaded")
     if args.gpu:
         set_gpu_mode(True)
         policy.cuda()
-    while True:
+    paths = []
+    while len(paths) < args.num_path:
         path = rollout(
             env,
             policy,
             max_path_length=args.H,
             render=True,
         )
+        paths.append(path)
         if hasattr(env, "log_diagnostics"):
             env.log_diagnostics([path])
         logger.dump_tabular()
 
+    if args.visualize:
+        plot_problem_paths(env, paths, fig_dir, show_fig=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -35,6 +44,10 @@ if __name__ == "__main__":
     parser.add_argument('--H', type=int, default=300,
                         help='Max length of rollout')
     parser.add_argument('--gpu', action='store_true')
+    parser.add_argument('-n', '--num-path', type=int, default=10,
+                        help='Number of paths to simulate.')
+    parser.add_argument('-v', '--visualize', action='store_true',
+                        help='Whether to visualize paths.')
     args = parser.parse_args()
 
     simulate_policy(args)
